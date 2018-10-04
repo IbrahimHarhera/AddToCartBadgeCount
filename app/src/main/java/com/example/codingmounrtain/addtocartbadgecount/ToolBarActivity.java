c4d7742b8b2a884eda46cc8ad618b87441613c61
Ibrahim Elsaid <ibrahim.elsaid@mediapanarab.com>
2018-10-04 14:52:26 +0200

    Add some files

---
+++ app/src/main/java/com/example/codingmounrtain/addtocartbadgecount/ToolBarActivity.java
@@ -0,0 +1,59 @@
+package com.example.codingmounrtain.addtocartbadgecount;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ToolBarActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView mTitle = toolbar.findViewById(R.id.toolbar_title);
+        toolbar.setTitle("ToolBar Activity");
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_menu));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //What to do on back clicked
+                Toast.makeText(ToolBarActivity.this, "Menu clicked.!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.cart_action);
+        menuItem.setIcon(Converter.convertLayoutToImage(ToolBarActivity.this, 1, R.drawable.ic_shopping_cart_white_24dp));
+        MenuItem menuItem2 = menu.findItem(R.id.notification_action);
+        menuItem2.setIcon(Converter.convertLayoutToImage(ToolBarActivity.this, 2, R.drawable.ic_notifications_white_24dp));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
